798081e2af3478e6835749d085595462b5890de1
kikifachrizi <kikifachrizi235@gmail.com>
2024-11-07 01:56:46 +0700

    update

--- code/ArduinoCore-arc32-master/libraries/CurieSerialFlash/CurieSerialFlash.h
+++
@@ -1,4 +0,0 @@
-#if !defined (__arc__)
-#error These examples only work with onboard SPI flash on Arduino/Genuino 101 board
-#endif
-#include "SerialFlash.h"
--- code/TA_delivFood_2_8_07_2024/BUILD/NUCLEO_F411RE/ARMC6/mbed-os/drivers/source/TableCRC.d
+++
@@ -1,4 +0,0 @@
-BUILD\NUCLEO_F411RE\ARMC6\mbed-os\drivers\source\TableCRC.o: \
-  mbed-os\drivers\source\TableCRC.cpp \
-  BUILD\NUCLEO_F411RE\ARMC6\mbed_config.h \
-  mbed-os\drivers\internal\TableCRC.h
